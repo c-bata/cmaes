87e5029ae4b555c055d8040e430dc71a69fd7817
c-bata <contact@c-bata.link>
2020-04-06 10:07:04 +0900

    Monkeypatch for faster intersection search space

---
+++ cmaes/monkeypatch.py
@@ -0,0 +1,24 @@
+from typing import Tuple
+
+try:
+    import optuna
+except:  # noqa: E722
+    optuna = None
+
+
+def get_optuna_version() -> Tuple[int, int]:
+    optuna_ver_info = optuna.__version__.split(".")
+    major_ver, minor_ver = int(optuna_ver_info[0]), int(optuna_ver_info[1])
+    return major_ver, minor_ver
+
+
+def patch_fast_intersection_search_space() -> None:
+    if optuna is None:
+        return
+
+    if get_optuna_version() != (1, 3):
+        return
+
+    from .sampler import _fast_intersection_search_space
+
+    optuna.samplers.intersection_search_space = _fast_intersection_search_space
